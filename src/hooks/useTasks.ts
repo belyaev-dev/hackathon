58f2a877946665d63ef66aee875c62b1de99fe2b
Denis Belyaev <d.belyaev@nwamo.ru>
2023-02-19 09:44:25 +0300

    check

--- src/hooks/useTasks.ts
+++ src/hooks/useTasks.ts
@@ -29,6 +29,7 @@ export const useTasks = () => {
    */
   const fetchAllTasks = async () => {
     const { data } = await useAxios("tasks", { method: "GET" }, instance);
+    console.log(data)
     if (data.value.count > 0) state.listAllTasks = data.value;
   };
 
