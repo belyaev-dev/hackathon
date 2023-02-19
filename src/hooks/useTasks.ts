aa044623da55c73bbae196445620b9eb365fb34a
Denis Belyaev <d.belyaev@nwamo.ru>
2023-02-19 09:20:06 +0300

    update Task interface

--- src/hooks/useTasks.ts
+++ src/hooks/useTasks.ts
@@ -1,48 +1,54 @@
-import { useAxios } from '@vueuse/integrations/useAxios'
-import { instance } from '../api/apiCreate'
-import {reactive} from 'vue'
-
+import { useAxios } from "@vueuse/integrations/useAxios";
+import { instance } from "../api/apiCreate";
+import { reactive } from "vue";
 
 interface Task {
-    status: string;
-    title: string;
-    description: string;
+  id: number;
+  status: string;
+  title: string;
+  description: string;
+  estateId?: number;
+  creationDate: Date;
 }
 
 interface TaskState {
-    listAllTasks: Task[];
-    titleTask: any;
-    descriptionTask: any;
+  listAllTasks: Task[];
+  titleTask: any;
+  descriptionTask: any;
 }
 
 export const useTasks = () => {
+  const state: TaskState = reactive({
+    listAllTasks: [],
+    titleTask: null,
+    descriptionTask: null,
+  });
 
+  /**
+   * Получение всех задач по фильтру
+   */
+  const fetchAllTasks = async () => {
+    const { data } = await useAxios("tasks", { method: "GET" }, instance);
+    if (data.value.count > 0) state.listAllTasks = data.value;
+  };
 
-    const state: TaskState = reactive({
-        listAllTasks: [],
-        titleTask: null,
-        descriptionTask: null,
-    })
+  /**
+   * Создание задачи
+   */
+  const createTask = async () => {
+    await useAxios(
+      "tasks",
+      {
+        method: "POST",
+        data: { title: state.titleTask, description: state.descriptionTask },
+      },
+      instance
+    );
+  };
 
-    /**
-     * Получение всех задач по фильтру
-     */
-    const fetchAllTasks = async () => {
-
-        const {data} = await useAxios('tasks', {method: 'GET'}, instance)
-        state.listAllTasks = data.value
-    }
-
-    /**
-     * Создание задачи
-     */
-    const createTask = async () => {
-        await useAxios('tasks', {method: 'POST', data: {title: state.titleTask, description: state.descriptionTask}}, instance)
-    }
-
-    return {
-        state,
-        fetchAllTasks,
-        createTask
-    }
-}
+  return {
+    state,
+    fetchAllTasks,
+    createTask,
+  };
+};
